f40e15c0f093cc54ec0bf6eeeddbe10415db9fec
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-04-07 15:04:39 +0000

    Create bisiesto.py

---
+++ 08 - Numbers/bisiesto.py
@@ -0,0 +1,15 @@
+
+
+def check_leap_year(year):
+    leap = False
+    if not year % 400:
+        leap = True
+    elif not year % 4 and year % 100:
+        leap = True
+    
+    print(f'{year} is leap year' if leap else f'{year} is not leap year')
+
+
+years = [1800, 1900, 1903, 2000, 2002]
+for year in years:
+    check_leap_year(year)
