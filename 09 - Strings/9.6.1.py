ec85da8f6ccf0512b5b3cf69dc2610ff6c5df863
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-04-10 19:56:45 +0000

    Update 9.6.1.py

--- 09 - Strings/9.6.1.py
+++ 09 - Strings/9.6.1.py
@@ -3,6 +3,6 @@ age = 319
 
 print(f"Tu nombre es {name} y tu edad {age}")
 
-# Tambien se puede formatear cadenas seguidas de colon
+# Tambien se puede formatear cadenas seguidas de :
 
 print(f"Raiz cuadrada de 2: {2**.5:5.3f}")
