0d9779a253b34b8d0c5ec2c12ef8e8c8628737d2
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-04-12 15:28:57 +0000

    Create 6.8.2.py

---
+++ 06_Variables/6.8.2.py
@@ -0,0 +1,10 @@
+# Calcular perimetro de un rectangulo
+
+def perimetro(altura, base):
+
+    p = 2 * (base + altura)
+    print(f'Perimetro de un rectangulo con base {base} y altura {altura} = {p}')
+    
+    
+perimetro(10,2)
+perimetro(10,6)
