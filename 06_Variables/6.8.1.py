0c829abb8ba0f0eb68d4677d880bbc60ec16bb11
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-04-12 15:21:00 +0000

    Update 6.8.1.py

--- 06_Variables/6.8.1.py
+++ 06_Variables/6.8.1.py
@@ -1,1 +1,10 @@
-pi = 3.1452
+# Calcular area de circulo con pi
+
+# pi = 3.1415
+
+import math
+
+radio = float(input("Introduzca el radio del círculo en cm: "))
+area = math.pi * radio ** 2
+
+print("El área del círculo es:", area, "cm^2")
