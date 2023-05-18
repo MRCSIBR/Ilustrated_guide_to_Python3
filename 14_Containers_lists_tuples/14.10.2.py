fe15d33a2c12f5414f50cc607a74c9d9e2859262
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-05-18 10:52:51 +0000

    Create 14.10.2.py

---
+++ 14_Containers_lists_tuples/14.10.2.py
@@ -0,0 +1,10 @@
+# Crear una tupla y agregarla a lista people
+
+person = ('Juan', 'Dos', 30)
+
+
+people = []
+people.append(('Jose', 'Tres', 40))
+people.append(('Juan', 'Dos', 30))
+
+print(people)
