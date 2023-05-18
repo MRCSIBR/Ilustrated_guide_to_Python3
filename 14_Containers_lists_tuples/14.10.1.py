592872088b2fff571fa8dbcbd0db5e48861bc789
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-05-18 10:47:31 +0000

    Create 14.10.1.py

---
+++ 14_Containers_lists_tuples/14.10.1.py
@@ -0,0 +1,13 @@
+colegas = []
+
+colegas.append('Juan')
+colegas.append('Eduardo')
+colegas.append('Paola')
+
+
+
+print(colegas[:1])     # imprimir primer item en la lista
+print(colegas[1:2])    # segundo item en la lista 
+
+
+print(id(colegas))
