5bc88a251eba8417f2f197480527f3e40d1c0e1d
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-05-26 21:14:36 -0300

    Create 15.9.1.py

---
+++ 15_Iterators/15.9.1.py
@@ -0,0 +1,10 @@
+# Ejercicio 15.9.1.py
+
+nombres = ["Alicia", "Juan", "Peter", "Donald"]
+
+total_length = sum(len(nombre) for nombre in nombres)
+average_length = total_length / len(nombres)
+
+print("The average length of names is:", average_length)
+
+  
