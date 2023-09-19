08ccff3c9af14ce667d58dcdd708e663e74864b3
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2023-09-19 04:41:55 -0300

    Create Chairlift.py
    Primer ejemplo del capitulo

---
+++ 21_Classes/Chairlift.py
@@ -0,0 +1,36 @@
+class Chairlift:
+    ''' Clase Chairlift: modela un funicular de 4 pasajeros
+        
+        Metodos: load_passenger
+                unload_passenger    
+    '''
+    
+    def __init__(self):
+        self.max_occupancy = 4
+        self.current_occupancy = 0
+    
+    def load_passenger(self):
+        if self.current_occupancy < self.max_occupancy:
+            self.current_occupancy += 1
+            print("Passenger loaded.")
+        else:
+            print("Chairlift is already full.")
+    
+    def unload_passenger(self):
+        if self.current_occupancy > 0:
+            self.current_occupancy -= 1
+            print("Passenger unloaded.")
+        else:
+            print("Chairlift is already empty.")
+            
+
+silla1 = Chairlift()
+
+# Cargar pasajeros
+silla1.load_passenger() 
+silla1.load_passenger()            
+
+# Descargar pasajeros
+silla1.unload_passenger()            
+silla1.unload_passenger()
+silla1.unload_passenger()            
